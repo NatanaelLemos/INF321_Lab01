4a7a2114d128e868f7f1874d56829bcecdb10cc4
Natanael Lemos <Natanael Lemos>
2016-10-01 10:04:03 -0300

    Teste passou

---
+++ src/main/java/BookstoreModel/BookstoreModel.java
@@ -0,0 +1,241 @@
+package BookstoreModel;
+
+import br.com.unicamp.inf321.BookStore;
+
+public class BookstoreModel extends org.graphwalker.core.machine.ExecutionContext implements BookStore{
+
+	@Override
+	public void visualizar_carrinho() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Dados_Preenchidos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enviarRetorno() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciarCompra() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void salvar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void SepararProduto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void EmSeparacao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decrementar_item() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void TelaLogin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preencherDados() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Pesquisa_preenchida() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void voltar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void incrementar_item() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selecionaEndereco() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fechar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Pgto_cartao_selecionado() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Pagamento_sucesso() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void TelaModalidadeEntregas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void preencherPesquisa() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void iniciar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Carrinho_de_compras() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void TelaEnderecosEntrega() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void adicionar_ao_carrinho() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selecionaModalidade() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aprovar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Dados_enviados_ipo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pesquisa() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Email_Enviado() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void TelaInclusaoEndereco() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pesquisar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void forcaErroEndereco() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void incluirEndereco() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ExibirRetorno() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Lista_de_produtos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enviarEmail() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Pagina_inicial() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void enviarDados() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Popup_de_erro() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void TelaResultadoCompra() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
